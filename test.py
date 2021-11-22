4f62c1211a5ba781e9b5fd94959a8c7432f467c9
Anna Lebowsky <a.lebowsky@posteo.de>
2021-11-22 15:48:10 +0100

    function phil added

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 print("Hallo")
 
 print("Philip")
+
+def phil_Weibull_Cal(wind_speed, a, k = 2):
+    WeiDist = k / a * (wind_speed/a)**(k-1)*np.exp(-(wind_speed/a)**k)
+    return WeiDist
+    
+    
